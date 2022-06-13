b46124758919a454a0bbfc04d5d702f87869292d
Gaurav Kabra <gauravkab07@gmail.com>
2022-06-13 12:38:49 +0530

    The Redesign Begins

--- project/models.py
+++
@@ -1,8 +0,0 @@
-from . import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True)
-    password = db.Column(db.String(30))
-    email = db.Column(db.String(30))
-    
---
+++ project/static/loginpage.js
@@ -0,0 +1,68 @@
+const student_btn = document.getElementById("student-btn");
+const teacher_btn = document.getElementById("teacher-btn");
+const admin_btn = document.getElementById("admin-btn");
+const loginBtn = document.querySelectorAll(".login-btn");
+const pathSelector = document.getElementById("login-btn-list");
+const studentLoginBox = document.getElementById("student-login-box");
+const teacherLoginBox = document.getElementById("teacher-login-box");
+const adminLoginBox = document.getElementById("admin-login-box");
+const loginBack = document.getElementById("login-back");
+const loginTypeText = document.getElementById("login-type-text");
+
+function fade(element) {
+  // var op = 1;  // initial opacity
+  // element.style.position = 'absolute';
+  // var timer = setInterval(function () {
+  //     if (op <= 0.1){
+  //         clearInterval(timer);
+  //         element.style.display = 'none';
+  //     }
+  //     element.style.opacity = op;
+  //     element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+  //     op -= op * 0.1;
+  // }, 50);
+  element.style.display = "none";
+}
+
+function unfade(element) {
+  var op = 0.1; // initial opacity
+  element.style.opacity = op;
+  element.style.display = "flex";
+  var timer = setInterval(function () {
+    if (op >= 1) {
+      clearInterval(timer);
+    }
+    element.style.opacity = op;
+    element.style.filter = "alpha(opacity=" + op * 100 + ")";
+    op += op * 0.1;
+  }, 10);
+}
+
+loginBtn.forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    console.log(btn);
+    fade(pathSelector);
+    unfade(loginBack);
+    if (btn.id == "student-btn" || btn.id == "student-login-btn") {
+      unfade(loginBack);
+      unfade(studentLoginBox);
+    }
+    if (btn.id == "admin-btn" || btn.id == "admin-login-btn") {
+      unfade(loginBack);
+      unfade(adminLoginBox);
+    }
+    if (btn.id == "teacher-btn" || btn.id == "teacher-login-btn") {
+      unfade(loginBack);
+      unfade(teacherLoginBox);
+    }
+    loginBack.style.display = "block";
+  });
+});
+
+loginBack.addEventListener("click", function () {
+  fade(studentLoginBox);
+  fade(teacherLoginBox);
+  fade(adminLoginBox);
+  fade(loginBack);
+  unfade(pathSelector);
+});
