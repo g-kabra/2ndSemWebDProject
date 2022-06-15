948d6d8e91c30f90b6c3bfcb0420435c5cf35d47
Gaurav Kabra <gauravkab07@gmail.com>
2022-06-15 11:07:46 +0530

    Added base files and login feature

--- project/auth.py
+++ project/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import Student
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -15,3 +17,35 @@ def signup():
 def logout():
     return 'Logout'
 
+@auth.route('/signup/student', methods=['POST'])
+def signupStudent():
+    fname = request.form['fname']
+    lname = request.form['lname']
+    branch = request.form['branch']
+    year = request.form['year']
+    email = request.form['email']
+    password = request.form['password']
+    # rollno = request.form['rollno']
+    
+    student = Student.query.filter_by(email = email).first()
+    
+    if student:
+        return "Kya be"
+    
+    new_user = Student(email = email, fname = fname, lname = lname, branch = branch, year = year, password = generate_password_hash(password, method='sha256'))
+    
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/signup')
+
+@auth.route('/login/student', methods = ['POST'])
+def loginStudent():
+    email = request.form['email']
+    password = request.form['password']
+    
+    student = Student.query.filter_by(email=email).first()
+    
+    if not student or not check_password_hash(student.password, password):
+        return "Kya be"
+    
+    return render_template('/student_profile.html', email = student.email, name = student.fname + " " + student.lname, branch = student.branch)
--- project/models.py
+++ project/models.py
@@ -3,8 +3,9 @@ from . import db
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(30), unique=True)
-    rollno = db.Column(db.Integer)
+    # rollno = db.Column(db.String(30))
     fname = db.Column(db.String(30))
     lname = db.Column(db.String(30))
     branch = db.Column(db.String(4))
     year = db.Column(db.Integer)
+    password = db.Column(db.String(30))
