6b0bb991c47d4c9f49a55feaddf5196e5363e9a3
Gaurav Kabra <gauravkab07@gmail.com>
2022-07-14 19:24:29 +0530

    Added excel file integration

--- app/auth.py
+++ app/auth.py
@@ -1,22 +1,26 @@
-from flask import Blueprint, render_template, redirect, request, session
+from flask import Blueprint, render_template, redirect, request, session, send_file
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import Student, Teacher, Admin
 from flask_login import login_user, login_required, logout_user, current_user
 from .main import profile
 from . import db
+import pandas as pd
 
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login')
 def login():
     return render_template('loginpage.html')
 
-@auth.route('/signup', methods = ['POST', 'GET'])
+
+@auth.route('/signup', methods=['POST', 'GET'])
 @login_required
 def signup():
     if (current_user.role == 'admin'):
         return render_template('signuppage.html')
     return profile()
+
 
 @auth.route('/logout')
 @login_required
@@ -24,6 +28,7 @@ def logout():
     print("logged out", current_user)
     logout_user()
     return render_template('loginpage.html')
+
 
 @auth.route('/signup/student', methods=['POST'])
 @login_required
@@ -36,18 +41,54 @@ def signupStudent():
         email = request.form['email']
         password = request.form['password']
         rollno = request.form['rollno']
-        
-        student = Student.query.filter_by(email = email).first()
-        
+
+        student = Student.query.filter_by(email=email).first()
+
         if student:
             return "Kya be"
-        
-        new_user = Student(email = email, fname = fname, lname = lname, branch = branch, year = year, password = generate_password_hash(password, method='sha256'), rollno = rollno)
-        
+
+        new_user = Student(email=email, fname=fname, lname=lname, branch=branch, year=year,
+                           password=generate_password_hash(password, method='sha256'), rollno=rollno)
+
         db.session.add(new_user)
         db.session.commit()
         return redirect('/signup')
     return profile()
+
+
+@auth.route('/signup/student-signup-excel', methods=['POST'])
+def admin_student_signup_excel():
+    information = request.files['excel-file']
+    object = pd.read_excel(information)
+    for i in range(len(object[object.keys()[0]])):
+        # print(object.iloc[i])
+        email = object.iloc[i]["email"]
+        fname = object.iloc[i]["fname"]
+        lname = object.iloc[i]["lname"]
+        branch = object.iloc[i]["branch"]
+        year = int(object.iloc[i]["year"])
+        print(year)
+        password = object.iloc[i]["password"]
+        print(password)
+        rollno = object.iloc[i]["rollno"]
+        student = Student.query.filter_by(email=email).first()
+
+        if student:
+            db.session.delete(student)
+            db.session.commit()
+        new_user = Student(email=email, fname=fname, lname=lname, branch=branch, year=year,
+                           password=generate_password_hash(password, method='sha256'), rollno=rollno)
+        db.session.add(new_user)
+    db.session.commit()
+    return profile()
+
+@auth.route('/signup/student-signup-excel-download', methods=['POST'])
+def admin_student_signup_download():
+    keys = {"email": [], "rollno": [], "fname": [],"lname": [],"branch": [],"year": [],"password": [] }
+    df = pd.DataFrame(data=keys)
+    df.to_excel("./app/templates/test1.xlsx")
+    # print(df)
+    return send_file("./templates/test1.xlsx", attachment_filename='signup_template.xlsx')
 
 @auth.route('/signup/teacher', methods=['POST'])
 @login_required
@@ -58,49 +99,53 @@ def signupTeacher():
         email = request.form['email']
         password = request.form['password']
         # rollno = request.form['rollno']
-        
-        teacher = Teacher.query.filter_by(email = email).first()
-        
+
+        teacher = Teacher.query.filter_by(email=email).first()
+
         if teacher:
             return "Kya be"
-        
-        new_user = Teacher(email = email, fname = fname, lname = lname, password = generate_password_hash(password, method='sha256'))
-        
+
+        new_user = Teacher(email=email, fname=fname, lname=lname,
+                           password=generate_password_hash(password, method='sha256'))
+
         db.session.add(new_user)
         db.session.commit()
         return redirect('/signup')
     return profile()
 
+
 @auth.route('/signup/admin', methods=['POST'])
 # @login_required
 def signupAdmin():
     # if (current_user.role == 'admin'):
-        fname = request.form['fname']
-        lname = request.form['lname']
-        dept = request.form['dept']
-        designation = request.form['designation']
-        email = request.form['email']
-        password = request.form['password']
-        
-        admin = Admin.query.filter_by(email = email).first()
-        
-        if admin:
-            return "Kya be"
+    fname = request.form['fname']
+    lname = request.form['lname']
+    dept = request.form['dept']
+    designation = request.form['designation']
+    email = request.form['email']
+    password = request.form['password']
 
-        new_admin = Admin(email = email, fname = fname, lname = lname, dept = dept, designation = designation, password = generate_password_hash(password, method='sha256'))
-        
-        db.session.add(new_admin)
-        db.session.commit()
-        return redirect('/signup')
+    admin = Admin.query.filter_by(email=email).first()
+
+    if admin:
+        return "Kya be"
+
+    new_admin = Admin(email=email, fname=fname, lname=lname, dept=dept,
+                      designation=designation, password=generate_password_hash(password, method='sha256'))
+
+    db.session.add(new_admin)
+    db.session.commit()
+    return redirect('/signup')
     # return profile()
-    
-@auth.route('/login/student', methods = ['POST'])
+
+
+@auth.route('/login/student', methods=['POST'])
 def loginStudent():
     email = request.form['email']
     password = request.form['password']
-    
+
     student = Student.query.filter_by(email=email).first()
-    
+
     if not student or not check_password_hash(student.password, password):
         return "Kya be"
     session['role'] = 'student'
@@ -108,13 +153,14 @@ def loginStudent():
     print("Logged in", current_user)
     return profile()
 
-@auth.route('/login/teacher', methods = ['POST'])
+
+@auth.route('/login/teacher', methods=['POST'])
 def loginTeacher():
     email = request.form['email']
     password = request.form['password']
-    
+
     teacher = Teacher.query.filter_by(email=email).first()
-    
+
     if not teacher or not check_password_hash(teacher.password, password):
         return "Kya be"
     session['role'] = 'teacher'
@@ -122,20 +168,18 @@ def loginTeacher():
     print("Logged in", current_user)
     return profile()
 
-@auth.route('/login/admin', methods = ['POST'])
+
+@auth.route('/login/admin', methods=['POST'])
 def loginAdmin():
     email = request.form['email']
     password = request.form['password']
-    
+
     admin = Admin.query.filter_by(email=email).first()
-    
+
     if not admin or not check_password_hash(admin.password, password):
         return "Kya be"
-    
-    
+
     session['role'] = 'admin'
     login_user(admin)
     print("Logged in", current_user)
     return profile()
-
-
